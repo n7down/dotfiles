9c9429ccf04a688ed9d1526574c7e1449bac2bed
Joshua Adams <n7down@gmail.com>
2018-02-13 06:32:01 +0000

    updated scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # make symbolic links to home folder
 link_file() {
@@ -7,40 +7,49 @@ link_file() {
 
 	if [ -e $target ] ; then
 		if [ ! -d $target ] ; then
-			echo "Update\t$target"
+			echo "Update $target"
 			mv $target $target.bak
 			ln -sf ${source} ${target}
 		fi
 	else
-		echo "Install\t$target"
+		echo "Install $target"
 		ln -sf ${source} ${target}
 	fi
 }
 
+# install files
+echo "installing basic file"
+apt install -y curl zsh tmux
+
+# install files for polybar
+echo "installing files for polybar"
+apt install -y cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev
+
+# change default shell to zsh
+if [ $SHELL != "/bin/zsh" ]
+then
+	echo "setting shell to zsh"
+	chsh -s /bin/zsh
+fi
+
+# install vundle
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]
+then
+	echo "installing vundle"
+	eval 'git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
+fi
+
+# install oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]
+then
+	echo "installing oh-my-zsh"
+	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+echo "creating/updating symlinks"
 for i in _*
 do
 	link_file $i
 done
 
-# change default shell to zsh
-#if [ ! $SHELL -eq "/bin/zsh" ]
-#then
-#	chsh -s /bin/zsh
-#fi
-
-# install vundle
-#if [ ! -d $HOME/.vim/bundle/Vundle.vim ]
-#then
-#	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim`
-#fi
-
-# install oh-my-zsh
-#if [ ! -d $HOME/.oh-my-zsh ]
-#then
-#	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#fi
-
 # install oh-my-zsh themes
-
-# install wallpapers
-
