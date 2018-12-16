4a5723fe3efe979dc67f13f08a67b8cb4f20b6d5
Joshua Adams <n7down@gmail.com>
2018-12-16 15:51:15 -0700

    updated wallpapers and scripts

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ then
 fi
 
 # install zsh-autosuggestions
+echo "install zsh-autosuggestions"
 sh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 # install vundle
