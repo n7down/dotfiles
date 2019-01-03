446b2d0c3a1bc7f58e190e84adccaf05f2f17a91
Joshua Adams <n7down@gmail.com>
2019-01-03 10:28:04 -0700

    added brightlight

---
+++ scripts/install-backlight.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+install_packages() {
+	sudo apt install -y libbsd-dev
+}
+
+install_backlight() {
+	mkdir -p ~/Source
+	cd Source
+	git clone https://github.com/multiplexd/brightlight.git
+	cd brightlight
+	make
+
+	# make symlink to ~/.bin folder
+	mkdir -p ~/.bin
+	ln -sf ${PWD} ${HOME}/.bin
+}
