5a449062253ffddd57c49cb84a666629deb32e09
Joshua Adams <n7down@gmail.com>
2019-04-09 09:02:08 -0600

    fixed scripts

--- _bin/sonos-scripts/set-volume.js
+++ _bin/sonos-scripts/set-volume.js
@@ -4,10 +4,10 @@ const { Sonos } = require('sonos')
 const device = new Sonos('192.168.1.209')
 
 const args = require('minimist')(process.argv.slice(2));
-console.log("args: %j", args)
+console.log("args: %j", args.s)
 
 // FIXME: get the volume as a param
-device.setVolume(args).then(result => {
+device.setVolume(args.s).then(result => {
 	  console.log("Set volume %j", result)
 }).catch(err => {
 	  console.log('Error setting volume %j', err)
