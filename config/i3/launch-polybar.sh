a40542541060b8c5f44ea34ac4f220b7a02377a4
Joshua Adams <n7down@gmail.com>
2019-04-22 15:57:00 -0600

    trying to change polybar settings for different monitors - not working

--- config/i3/launch-polybar.sh
+++ config/i3/launch-polybar.sh
@@ -9,9 +9,17 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 #polybar bar &
 
+SECONDARY_MONITOR_1="DP-1-1"
+SECONDARY_MONITOR_2="DP-1-2"
+
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload bar &
+		echo $m $MAIN_MONITOR
+		if [ $m == $SECONDARY_MONITOR_1 || $m == $SECONDARY_MONITOR_2 ]; then 
+			MONITOR=$m polybar --reload bar-sec &
+		else
+			MONITOR=$m polybar --reload bar &
+		fi
 	done
 else
 	polybar --reload bar &
