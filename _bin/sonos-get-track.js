1cf5355cf95e5e989f7a146507ed969b4900e046
Joshua Adams <n7down@gmail.com>
2019-05-09 14:03:42 -0600

    able to parse from terminal

--- _bin/sonos-get-track.js
+++ _bin/sonos-get-track.js
@@ -4,6 +4,6 @@ const { Sonos } = require('sonos')
 const device = new Sonos('192.168.1.209')
 
 device.currentTrack().then(track => {
-	  console.log(track)
+	  console.log(JSON.stringify(track, " ", 4))
 }).catch(err => { console.log('Error occurred %j', err) })
 
